e73a0885cc484431ebea5e4750bc005b6bb50024
romeromatias64 <matiasaromero03@gmail.com>
2025-05-06 22:55:56 -0300

    rutas

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -8,6 +8,17 @@ router.get("/products", (productController.getProducts))
 // Ruta para obtener un producto por ID
 router.get("/products/:id", productController.getProductByID)
 
+router.get("/uploads/products/:filename", (req, res) => {
+    const filename = req.params.filename
+    const path = `uploads/products/${filename}`
+    res.download(path, filename, (err) => {
+        if (err) {
+            console.log(err)
+            res.status(500).send("Error al descargar el archivo")
+        }
+    })
+})
+
 // Ruta para crear un producto
 router.post("/products", upload, productController.createProduct)
 
